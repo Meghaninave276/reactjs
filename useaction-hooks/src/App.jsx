675c0a2dab9603d54c16fac4db07cd06c3da20e0
Megha <meghaninave8@gmail.com>
2025-12-06 14:11:12 +0530

    ...

--- useaction-hooks/src/App.jsx
+++ useaction-hooks/src/App.jsx
@@ -4,20 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [data,action,pending]=useActionState([])
+  const handlesubmit=async(prevdata,formdata)=>{
+    let name=formdata.get('name');
+    let password=formdata.get('password');
+    await new Promise((res)=>setTimeout(res,2000));
+    console.log("first",name,password);
+    if(name && password){
+      return{message:"data submitted",name,password}
+    }
+    else{
+      return{error:"failed to submit"}
+    }
+  }
+  const [data,action,pending]=useActionState(handlesubmit,undefined);
+
  
 
   return (
     <>
      <h1>useaction</h1>
      <form action={action}>
-      <input type="text" placeholder='enter name' name='name' />
+      <input type="text" defaultValue={data?.name} placeholder='enter name' name='name' />
       <br /><br />
-       <input type="password" placeholder='enter password' name='password' />
+       <input type="password" defaultValue={data?.password} placeholder='enter password' name='password' />
       <br /><br />
-      <button>submit</button>
+      <button disabled={pending}>submit</button>
+      <br />
+      {
+        data?.error && <span style={{color:"red"}}>{data?.error}</span>
+      }
+      {
+         data?.message && <span style={{color:"green"}}>{data?.message}</span>
+      }
 
      </form>
+     <h3>name:{data?.name}</h3>
+     <h3>password:{data?.password}</h3>
     </>
   )
 }
