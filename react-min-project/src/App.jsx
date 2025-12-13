9998ac8341836f5b8409c07b35041838b9c906ba
Megha <meghaninave8@gmail.com>
2025-12-13 20:08:41 +0530

    ...

--- react-min-project/src/App.jsx
+++ react-min-project/src/App.jsx
@@ -4,30 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const colors=JSON.parse( localStorage.getItem("color"));
+  const [r,setR]=useState(colors && colors.r?colors.r:0);
+  const [g,setG]=useState(colors && colors.g?colors.g:0);
+  const [b,setB]=useState(colors && colors.b?colors.b:0);
+  const save=()=>{
+   
+    localStorage.setItem("color",JSON.stringify({r,g,b}));
+     console.log("save");
+  }
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Code mixer</h1>
+      <div style={{backgroundColor:'rgb('+r+', '+g+', '+b+')', height:200, width:200}}>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label htmlFor="">red</label>
+      <input type="range" value={r} onChange={(e)=>setR(e.target.value)} min={0} max={255}/>
+      <br /><br />
+      <label htmlFor="">green</label>
+      <input type="range" value={g} onChange={(e)=>setG(e.target.value)} min={0} max={255}/>
+      <br /><br />
+      <label htmlFor="">blue</label>
+      <input type="range" value={b} onChange={(e)=>setB(e.target.value)} min={0} max={255}/>
+      <br /><br />
+      <button onClick={save}>save color combination</button>
     </>
   )
 }
